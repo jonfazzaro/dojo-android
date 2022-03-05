99615fdcf75bfc403f60f2e3b0255d4fa1bd1e90
Jon Fazzaro <jon@fazzaro.com>
2022-03-05 12:32:33 -0500

    wip: basic Fizz Buzz form

--- app/src/main/java/com/fazzaro/dojo/android/FizzBuzzView.kt
+++ app/src/main/java/com/fazzaro/dojo/android/FizzBuzzView.kt
@@ -1,17 +1,22 @@
 package com.fazzaro.dojo.android
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
 import androidx.compose.material.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.*
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.MutableLiveData
+import com.fazzaro.dojo.android.ui.models.FizzBuzzViewModel
 import com.fazzaro.dojo.android.ui.theme.DojoTheme
 
-class FizzBuzzView {
+class FizzBuzzView() {
 
     @Composable
     fun Render() {
+        var input by remember { mutableStateOf("") }
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
@@ -21,12 +26,12 @@ class FizzBuzzView {
             Text("Let's play Fizz Buzz!")
             Spacer(modifier = Modifier.size(20.dp))
             OutlinedTextField(
-                value = "",
+                value = input,
                 label = { Text("Enter a number") },
-                onValueChange = {},
+                onValueChange = { input = it },
                 modifier = Modifier.fillMaxWidth()
             )
-            Text("(The result)" )
+            Text( "(No result)" )
             Spacer(modifier = Modifier.size(20.dp))
             Button(enabled = false, onClick = {}) {
                 Text(text = "Play")
