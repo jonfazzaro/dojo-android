7fa7db9ae5e88baa9dbe5099f8ee2739e46d0f8c
Jon Fazzaro <jon@fazzaro.com>
2022-03-07 09:38:45 -0500

    feat: numeric keypad

--- app/src/main/java/com/fazzaro/dojo/android/FizzBuzzView.kt
+++ app/src/main/java/com/fazzaro/dojo/android/FizzBuzzView.kt
@@ -1,9 +1,11 @@
 package com.fazzaro.dojo.android
 
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.*
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -26,9 +28,10 @@ class FizzBuzzView() {
                 value = "",
                 label = { Text("Enter a number") },
                 onValueChange = { },
-                modifier = Modifier.fillMaxWidth()
+                modifier = Modifier.fillMaxWidth(),
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
             )
-            Text("(No result)")
+            Text( "(No result)", )
             Spacer(modifier = Modifier.size(20.dp))
             Button(enabled = false, onClick = {}) {
                 Text(text = "Play")
